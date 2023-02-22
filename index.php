89739540aba5144f46daac4d8830fdf9b48bfe4e
maelbadet <98584091+maelbadet@users.noreply.github.com>
2023-02-23 00:03:10 +0100

    sauvegarde
    sauvegarde du 22/02/2023 contenant
    premiere page rédiger sans style,
    header paramétré, pages nommées et préremplis avec les infos.
    
    Il reste a faire :
    style sur la page d'accueil,
    remplir la page projet et DevPerso,
    faire le footer avec tous les liens vers les différents réseeaux sociaux et mentions légales,
    mettre la vidéo au centre

---
+++ index.php
@@ -0,0 +1,72 @@
+<!--
+===========================================================
+===========================================================
+===================  Exclusivité de  ======================
+===================   Badet Mael     ====================== 
+===========================================================
+===========================================================
+-->
+
+<body>
+<?php include('hf/header.php') ?>
+
+<!--test de l'image responsive pour occuper toute la taille de l'écran-->
+
+<img class="img-responsive" src="img/pc_background.jpg" style=width:100%>
+
+<article class="testimg">
+  <h2>Maël Badet</h2>
+  <p>étudiant en BTS SIO<p>
+</article>
+
+<!--test de l'image responsive pour occuper toute la taille de l'écran-->
+
+<!-- Div du a propos pour la présentation personnel-->
+<h2 class="pt-5">A propos de moi</h2>
+<div class="pt-3 border-bottom border-secondary"> 
+  <article class="d-flex flex-row justify-content-between" >
+    <p class="pr-2">mettre une photo</p>
+    <p class="w-50">Salut ! Je m'appelle Maël Badet, je suis un Développeur junior dans le domaine du web depuis plus de 4 ans,<br>
+      actuellement en deuxième année de BTSSIO, l'expérience que j'ai pus accumulé durant ce BTS m'offre des possibilités dans plusieurs domaines comme la mise 
+      en place de site internet statiques et dynamiques, mais aussi des applications. De plus je suis aussi cappable de paramétrer des routeurs et des switchs, 
+      et de réaliser des debugages de réseaux. Pour finir je peux aussi opérer sur la partie gestion des incidents. On peut donc dire que je suis quelqu'un de 
+      très polivalent, pouvant intervenir aussi bien sur la partie développement que la partie réseaux d'une entreprise. Pour plus de précision, vous pouvez vous
+      rendre sur la page de <a href="projet.php">mes projets </a>.
+    </p>
+  </article>
+</div>
+<!-- Fin du Div du a propos pour la présentation personnel-->
+
+<!--Div pour présentations de mes 3 plus gros projets-->
+<div class="pt-3 border-bottom border-secondary">
+<h2>quelques projets</h2>
+<div class="d-flex flex-row justify-content-around align-items-stretch pt-3">
+  <p>premier projet</p>
+  <p>deuxieme projet</p>
+  <p>troisieme projet</p>
+</div>
+</div>
+<!--Fin du Div pour présentations de mes 3 plus gros projets-->
+
+<div class=" pt-3 border-bottom border-secondary">
+  <h2>pourquoi avoir une Présence en ligne ?</h2>
+  <!--ici seras la vidéo contenant les expllications pour la question de la présence en ligne-->
+  <iframe src="video/VALORANT_replay_2023.02.01-16.42.mp4" frameborder="0" class="ratio ratio-1x1 center-block "></iframe>
+  <!--fin de la présentation orale-->
+
+  <!--ici seras le texte explicatif de la raison d'avoir une présence en ligne-->
+  <p>mettre le texte d'explication de l'importance de la préxence en ligne d'une entreprise</p>
+  <!--fin de l'explication de la présence en ligen de l'entreprise-->
+</div>
+
+<div class="pt-3 border-bottom border-secondary">
+  <h2>A voir aussi </h2>
+  <div>
+    <p>présentation détaillé</p>
+    <p>projets</p>
+    <p>contact</p>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ projet.php
@@ -0,0 +1,24 @@
+<!--Dans cette partie il faut mettre les points suivants :
+    - Gestion du patrimoine
+    - Gestion des incidents
+    - Gestion de projet
+    - services aux utilisateurs
+    - projets SLAM
+    - Projet SISR
+    - Projet personnel
+    - Stages-->
+
+
+<!--
+===========================================================
+===========================================================
+===================  Exclusivité de  ======================
+===================   Badet Mael     ====================== 
+===========================================================
+===========================================================
+-->
+
+
+<body>
+    <?php include('hf/header.php')  ?>
+</body>
