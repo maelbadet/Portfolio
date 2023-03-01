bcb4a501aa7cb8cbf70753c86bd62693463b8f29
Mael BADET <mbadet@B409.BTSSIO.FR>
2023-03-01 09:53:05 +0100

    presentation de la page description personnelle

--- DevPerso.php
+++ DevPerso.php
@@ -8,5 +8,34 @@
 -->
 
 <body>
+    <div>
     <?php include('hf/header.php')  ?>
+    </div>
+
+    <!--présentation du cursus détaillé -->
+
+    <h2 class="pt-5">cursus scolaire</h2>
+    <p>mettre ici les différents cursus scolaire (lycée,IUT,BTS, et études sup)</p>
+
+    <!--présentation du futur métier choisis-->
+    <h2>métier a l'avenir</h2>
+    <p>mettre ici le métier à venir</p>
+    <p>pourquoi ce choix de métier</p>
+
+    <!--présentations des infos importer par le bts sur ces 2 années-->
+    <h2>Ce que j'ai acquis durant mon BTS</h2>
+    <p>découper les parties slam et sisr puis mettre les points en mettants une barre de maîtrise</p>
+    <p>mettre le premier et le deuxième stage aussi</p>
+    
+    <!--veille informationnelle-->
+    <h2>Veille informationnelle</h2>
+    <p>mettre ici la vidéo de la veille informationnelle</p>
+    <p>mettre ici le texte de présentation de la vidéo de la veille informationnelle</p>
+
+    <!--mettre le cv en mode consultable ou bien meme en mode téléchargement-->
+    <h2>Mon CV</h2>
+    <p>mettre ici le CV</p>
+    <a href="#">mettre ici le liens de visionnage</a>
+    <a href="#">mettre le nom du fichier a telecharger</a>
+
 </body>
