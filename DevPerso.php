0ee2e4c62c907327b62747030bf30321bd2755f7
Mael BADET <mbadet@B409.BTSSIO.FR>
2023-03-08 10:07:53 +0100

    sauvegarde page projets et presentation détaillé

--- DevPerso.php
+++ DevPerso.php
@@ -14,8 +14,45 @@
 
     <!--présentation du cursus détaillé -->
 
-    <h2 class="pt-5">cursus scolaire</h2>
+    <h2 class="pt-5 mt-5">cursus scolaire</h2>
     <p>mettre ici les différents cursus scolaire (lycée,IUT,BTS, et études sup)</p>
+    <h3>Bac STI2D option SIN (Angoulême 2017-2020)</h3>
+    <div class = "d-flex flex-row justify-content-around border-bottom border-secondary align-items-center">
+        <img src="https://www.cfa-acad-poitiers.fr/sites/default/files/styles/visuel_page_etablissement/public/le-lycee-charles-de-coulomb-tire-son-epingle-du-jeu.jpg?itok=RPrp-SOj" alt="#" class ="w-75">
+        <p class = "w-50 pl-5">j'ai commencer la programmation lors de ma première année de STI2D en option SIN (système d'information et numérique) où j'ai appris
+            la base du codage en C avec de la programmation d'Arduino et de petits projets comme des labyrinthes ou des gestions d'écrans de parking. C'est donc la bas 
+            que j'ai obtenu mon <strong> bac sti2d mention assez bien et section européene</strong>. D'où j'ai ensuite été en étude supérieur.
+            </p>
+    </div>
+
+    <h3>Réseaux et Télécommunication (La Rochelle 2020-2021)</h3>
+    <div class = "d-flex flex-row justify-content-around border-bottom border-secondary align-items-center">
+        <img src="https://www.iut-larochelle.fr/wp-content/uploads/2021/03/departement-reseaux-et-telecommunications-2-1600x600.jpg" alt="" class="w-75">
+        <p class="w-50 pl-5">
+            Après mon bac je suis allé dans la partie Réseaux et télécoms de La Rochelle ayant hésiter entre la partie développement et la partie réseaux.
+            Lors de cette année, j'ai découvert les bases de la sécurité informatique de la télécommunication de signal et de la programmation en <strong>python</strong>
+            mais aussi les bases du développement web avec du <strong>html/css/php</strong>. J'ai pus m'appercevoir durant le cours de l'année, que le réseaux n'étais pas ce qui me convenait le mieux 
+            et j'ai donc décidé d'arrêter a la fin de ma première année et de me redirigé vers un BTS SIO qui était d'une part plus polyvalent en terme 
+            de compétences et surtout avec plus de pratique que de théorique, et une option SLAM pour faire du développement. 
+        </p>
+    </div>
+
+    <h3>BTS SIO option SLAM (lycée Elie Vinet 2021-2023)</h3>
+    <div class = "d-flex flex-row justify-content-around border-bottom border-secondary align-items-center">
+        <img src="https://www.cfa-acad-poitiers.fr/sites/default/files/styles/visuel_page_etablissement/public/vinet_0.jpg?itok=kDg7txte" alt="#" class="w-75">
+        <div>
+        <p class= "w-50 pl-5">
+            Je me suis donc redirigé vers un BTS SIO dans lequel durant le premier semestre, nous avons bordez les thèmes généraux en classe entières.
+            Durant ce premier semestre, on à abordé les thèmes suivants :</p>
+            <ul class= "w-50 pl-5">
+                <li>
+                    <p>le développement web : html/css/javascript/php/SQL</p>
+                    <p>le réseau : configuration routeur/switch, mise en place d'un réseaux, adressage ipV4, configuration apache</p>
+                </li>
+            </ul>
+        <p class= "w-50 pl-5">puis, nous nous sommes divisé en plusieurs classes, selons les option.</p>
+        </div>
+    </div>
 
     <!--présentation du futur métier choisis-->
     <h2>métier a l'avenir</h2>
--- hf/header.php
+++ hf/header.php
@@ -46,7 +46,7 @@
                     <a class="nav-link" href="projet.php">Projets <span class="sr-only">(current)</span></a>
                 </li>
                   <li class="nav-item active">
-                    <a class="nav-link" href="ServiceUtilisateurs.php">contact <span class="sr-only">(current)</span></a>
+                    <a class="nav-link" href="contact.php">contact <span class="sr-only">(current)</span></a>
                   </li>
               </ul>
             </div>
